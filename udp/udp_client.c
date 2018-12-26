fc8ba699536429350b61b96f538ad6e69b6bcb53
Praveen Kumar <praveenkumar@Praveens-MacBook-Air.local>
2018-12-26 11:45:49 +0530

    added udp client file

---
+++ udp/udp_client.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#define PORT 8080
+
+struct details{
+        int rollno;
+        char name[1024];
+        int dept;
+    };
+
+int main(int argc, char const *argv[])
+{
+    int clisock;
+    struct sockaddr_in client;
+    char buffer[1024];
+    socklen_t clientsize;
+    
+    struct details s;
+
+    if((clisock = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+        printf("socket connection failed");
+    }
+
+    bzero((struct *sockaddr)&client, sizeof(client));
+    client.sin_family = AF_INET;
+    client.sin_port = htons(PORT);
+    client.sin_addr.s_addr = htons(INADDR_ANY);
+
+    clientsize = sizeof(client);
+
+    do{
+            printf("Enter the name: ");
+            scanf("%s", s.name);
+
+            printf("Enter the roll no: ");
+            scanf("%d", &s.rollno);
+
+            printf("Enter the department: ");
+            scanf("%d", &s.dept);
+            
+            sendto(clisock, &s, sizeof(s), 0, (struct sockaddr*) &client, sizeof(client));
+            write(clisock, &s, sizeof(s));
+            listen(clisock,1);
+            
+            recvfrom(clisock, buffer, 1024, 0, (struct sockaddr*) &client, &clientSize);
+            read(clisock, buffer, sizeof(buffer));
+            printf("Server: %s\n", buffer);
+
+            printf("\npress 0 to exit");
+            printf("\n");
+            scanf("%d", &a);
+
+        }while( a != 0);
+
+    
+    close(client);
+    
+    return 0;
+}
+
